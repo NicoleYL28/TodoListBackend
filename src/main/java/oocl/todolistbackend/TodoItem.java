48d24ec0f8f94344622c65e875f580b422564350
TAMNI <tamni@oocl.com>
2025-09-16 14:05:48 +0800

    feat: add TodoItem entity and SQL migration for todos table

---
+++ src/main/java/oocl/todolistbackend/TodoItem.java
@@ -0,0 +1,24 @@
+package oocl.todolistbackend;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "todos")
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String text;
+    @Builder.Default
+    private boolean done = false;
+
+}
